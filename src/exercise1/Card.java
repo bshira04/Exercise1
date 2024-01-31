5c604a51ce860315d9eb734a82781c18892bf8ad
Bharat Shira <shirab@sheridancollege.ca>
2024-01-31 10:15:26 -0500

    working code

--- src/exercise1/Card.java
+++ src/exercise1/Card.java
@@ -7,7 +7,7 @@ package exercise1;
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
- * @author dancye
+ * @author Bharat Shira
  * @author Paul Bonenfant May 2020
  */
 public class Card {
@@ -23,6 +23,11 @@ public class Card {
         return suit;
     }
 
+    public Card(String suit, int value) {
+        this.suit = suit;
+        this.value = value;
+    }
+    
     /**
      * @param suit the suit to set
      */
